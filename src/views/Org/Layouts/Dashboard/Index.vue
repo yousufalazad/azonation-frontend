a7ccc06636ec104a8c0eb75dbf5caffdcc569961
Yousuf Al Azad <yousufalazad@gmail.com>
2024-08-25 21:40:14 +0100

    Dashboard design

--- src/views/Org/Layouts/Dashboard/Index.vue
+++ src/views/Org/Layouts/Dashboard/Index.vue
@@ -5,21 +5,44 @@ import Header from '../Partials/Header.vue';
 import LeftSidebar from '../../../Org/Layouts/Partials/LeftSidebar.vue';
 //import InitialContent from '@/views/Individual/Layouts/Dashboard/InitialContent.vue';
 </script>
-
 <template>
   <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-12 bg-primary top-header">
       <Header></Header>
     </div>
+  </div>
+  <div class="row">
+    <div class="col-md-2 bg-success left-sidebar">
+      <LeftSidebar></LeftSidebar>
 
-    <div class="row">
-      <div class="col-md-2">
-          <LeftSidebar></LeftSidebar>
-        </div>
-        <div class="col-md-10">
-          <router-view />
-        </div>
+    </div>
+    <div class="col-md-10 bg-danger main-content-area">
+      <router-view />
     </div>
     
   </div>
 </template>
+
+<style scoped>
+  .row {
+    margin: 0;
+  }
+  
+  .top-header {
+    height: 80px; /* Adjust the height of the top header as needed */
+  }
+
+  .left-sidebar {
+    position: fixed;
+    top: 80px; /* Adjust according to the top-header height */
+    bottom: 0;
+    overflow-y: auto;
+    height: calc(100vh - 80px); /* Full height minus the header */
+  }
+
+  .main-content-area {
+    margin-left: 16.6667%; /* Adjust according to the col-md-2 width */
+    padding: 20px; /* Optional: for some padding inside the content area */
+    padding-right: 50px;
+  }
+</style>
