33cde15ca563c5800260ae6450abc3c44d30cbb3
Yousuf Al Azad <ourazad@gmail.com>
2024-09-20 00:28:15 +0100

    Notification icon update

--- src/views/Org/Layouts/Partials/Header.vue
+++ src/views/Org/Layouts/Partials/Header.vue
@@ -4,7 +4,7 @@
     <div>
       <!-- Organization/Brand Name -->
       <a href="/org-dashboard/dashboard-initial-content" class="text-xl font-semibold text-gray-600">
-        {{ userName }}
+        {{ name }}
       </a>
     </div>
 
@@ -13,13 +13,13 @@
       <!-- Notifications -->
       <div class="relative">
         <button @click="toggleNotificationDropdown" class="relative text-gray-600" ref="notificationButton">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bell"
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bell mr-4 mt-2"
             viewBox="0 0 16 16">
             <path
               d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6" />
           </svg>
           <span v-if="unreadCount > 0"
-            class="absolute top-0 right-0 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">{{
+            class="absolute top-0 right-0 bg-blue-600 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center">{{
               unreadCount }}</span>
         </button>
 
@@ -93,8 +93,9 @@
               </button>
             </li>
             <li class="border-t"></li>
-            <li class="py-4 px-3.5"><img src="../../../../assets/Logo/Azonation.png" alt="" class="max-h-[50px] max-w-[100px]"></li>
-           
+            <li class="py-4 px-3.5"><img src="../../../../assets/Logo/Azonation.png" alt=""
+                class="max-h-[50px] max-w-[100px]"></li>
+
           </ul>
         </div>
       </div>
@@ -109,7 +110,7 @@ import { authStore } from "../../../../store/authStore";
 const auth = authStore;
 const userId = auth.user.id;
 const baseURL = 'http://localhost:8000';
-const userName = computed(() => auth.user?.name);
+const name = computed(() => auth.user?.name);
 const userType = computed(() => auth.user?.type);
 const userEmail = computed(() => auth.user?.email);
 import dayjs from 'dayjs';
